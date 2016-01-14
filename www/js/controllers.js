f7e8f681ecb365499e307ec7d594947aa3e64162
beatface <emma.beaton@gmail.com>
2016-01-14 13:11:25 -0600

    added ng-models and ng-click to add-note template and created addNoteToStorage function to add new note to local storage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,34 @@
 angular.module('app.controllers', [])
 
+//---------------------------------//
+//----- ADD NOTE PAGE CONTROL -----//
+//---------------------------------//
 .controller('pageCtrl', function($scope) {
   console.log("it's here");
+
+  $scope.addNoteToStorage = function(noteTitle, noteContent) {
+    var notesObj = JSON.parse(localStorage.getItem('notes')) || {};
+    var timeStamp = Date.now();
+    notesObj[timeStamp] = {
+      title: noteTitle,
+      note: noteContent
+    };
+    console.log("the new note is >>>>", notesObj);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+  };
+
+
 })
+
+// //---------------------------------//
+// //----- ADD NOTE PAGE CONTROL -----//
+// //---------------------------------//
+// .controller('pageCtrl', function($scope) {
+//   console.log("it's here");
+
+
+
+
+
+// })
 
