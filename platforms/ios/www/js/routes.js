da4183c267bfe5fdfa15e88620581202c9101afb
beatface <emma.beaton@gmail.com>
2016-01-15 13:06:25 -0600

    added dependencies

--- platforms/ios/www/js/routes.js
+++ platforms/ios/www/js/routes.js
@@ -1,37 +1,3 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-      
-    .state('menu', {
-      url: '/side-menu21',
-      abstract:true,
-      templateUrl: 'templates/menu.html'
-    })
-      
-    
-      
-        
-    .state('menu.page', {
-      url: '/page1',
-      views: {
-        'side-menu21': {
-          templateUrl: 'templates/page.html',
-          controller: 'pageCtrl'
-        }
-      }
-    })
-        
-      
-    ;
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/side-menu21');
-
-});
